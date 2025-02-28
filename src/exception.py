d5c25093522e884c4d5bb6f1e2f8deed3297fd61
astrooinaut <rooi860@gmail.com>
2025-02-28 19:09:54 +0200

    Added dataset

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     # Get the execution information, Krish Naik is uninterested in first 2 info, only 3rd
@@ -7,14 +8,14 @@ def error_message_detail(error, error_detail:sys):
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
     file_name, exc_tb.tb_lineno,str(error)
-
-    return error_message
     )
+    return error_message
+    
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
-        return self.error_message 
+        return self.error_message
